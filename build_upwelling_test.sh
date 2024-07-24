b04ce5386245f6856a68d8076c587da3b3c93eed
Anthony Meza <64243783+anthony-meza@users.noreply.github.com>
2024-07-24 16:41:24 -0400

    Update build_upwelling_test.sh

--- build_upwelling_test.sh
+++ build_upwelling_test.sh
@@ -2,3 +2,10 @@
 
 module load stack/impi
 module load stack/intel
+
+#this should be the directory that contains roms and roms_test directories
+# i.e. ROMS_ROOT_DIR/roms and ROMS_ROOT_DIR/roms_test
+ROMS_ROOT_DIR=/vortexfs1/home/anthony.meza/scratch 
+export ROMS_ROOT_DIR
+
+source build_roms.sh
